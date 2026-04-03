4cd32f50a8a2da24a19cede0f1488c98084c0ec2
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-04-03 21:13:29 +0800

    Refactor VM

---
+++ vm/src/VM.zig
@@ -0,0 +1,105 @@
+const std = @import("std");
+
+const debug = std.debug;
+const fmt = std.fmt;
+const heap = std.heap;
+const math = std.math;
+const mem = std.mem;
+
+const Var = union(VarType) { i64: i64, bool: bool };
+const VarType = enum { i64, bool };
+
+pub const Instruction = enum(u8) { add, push };
+
+const Self = @This();
+
+gpa: *heap.DebugAllocator(.{}),
+allocator: mem.Allocator,
+
+vars: std.ArrayList(Var),
+stack: std.ArrayList(Var),
+
+err_buf: [1024]u8,
+err: [*:0]const u8,
+
+pub fn init() Self {
+    const gpa = heap.c_allocator.create(heap.DebugAllocator(.{})) catch @panic("failed to create the allocator");
+    gpa.* = .init;
+    return .{
+        .gpa = gpa,
+        .allocator = gpa.allocator(),
+        .vars = .{},
+        .stack = .{},
+        .err_buf = [_]u8{0} ** 1024,
+        .err = "",
+    };
+}
+
+pub fn deinit(self: *Self) void {
+    self.vars.deinit(self.allocator);
+    self.stack.deinit(self.allocator);
+    debug.assert(self.gpa.deinit() == .ok);
+    heap.c_allocator.destroy(self.gpa);
+}
+
+pub fn execute(self: *Self, bytecode: []const u8) !void {
+    defer {
+        debug.print("vars: {any}\n", .{self.vars.items});
+        debug.print("stack: {any}\n", .{self.stack.items});
+    }
+
+    var i: usize = 0;
+    while (i < bytecode.len) : (i += 1) {
+        const instruction: Instruction = @enumFromInt(bytecode[i]);
+        switch (instruction) {
+            .add => {
+                const lhs = switch (self.pop()) {
+                    .i64 => |v| v,
+                    else => |v| {
+                        self.err = try fmt.bufPrintZ(
+                            &self.err_buf,
+                            "evaluating `add`: expecting `i64`, found `{t}`",
+                            .{v},
+                        );
+                        return error.Runtime;
+                    },
+                };
+                const rhs = switch (self.pop()) {
+                    .i64 => |v| v,
+                    else => |v| {
+                        self.err = try fmt.bufPrintZ(
+                            &self.err_buf,
+                            "evaluating `add`: expecting `i64`, found `{t}`",
+                            .{v},
+                        );
+                        return error.Runtime;
+                    },
+                };
+                debug.print("{}\n", .{lhs + rhs});
+            },
+            .push => {
+                try self.stack.append(
+                    self.allocator,
+                    self.vars.items[mem.readVarInt(u16, bytecode[i + 1 .. i + 3], .little)],
+                );
+                i += 2;
+            },
+        }
+    }
+}
+
+pub fn addVar(self: *Self, variable: Var) !u16 {
+    if (self.vars.items.len > math.maxInt(u16)) {
+        self.err = fmt.comptimePrint(
+            "adding variable to memory: max variable number exceeded, which is `{}`",
+            .{math.maxInt(u16)},
+        );
+        return error.MaxVariableNumberExceeded;
+    }
+    try self.vars.append(self.allocator, variable);
+    return @as(u16, @intCast(self.vars.items.len - 1));
+}
+
+fn pop(self: *Self) Var {
+    return self.stack.pop().?;
+}
