a836dc81e218924f6f070f7c12d75cf0189ce8cd
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-04-23 22:43:10 +0800

    VM: add load instruction and C API exports

--- vm/src/VM.zig
+++ vm/src/VM.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-
 const debug = std.debug;
 const fmt = std.fmt;
 const heap = std.heap;
@@ -9,40 +8,39 @@ const mem = std.mem;
 const Var = union(VarType) { i64: i64, bool: bool };
 const VarType = enum { i64, bool };
 
-pub const Instruction = enum(u8) { add, push };
+pub const Instruction = enum(u8) { add, push, load };
+
+const Error = error{ Runtime, MaxVariableNumberExceeded } || mem.Allocator.Error;
 
 const Self = @This();
+
+pub const max_slot_size = math.maxInt(u16);
 
 gpa: *heap.DebugAllocator(.{}),
 allocator: mem.Allocator,
 
-vars: std.ArrayList(Var),
-stack: std.ArrayList(Var),
+vars: std.ArrayList(Var) = .empty,
+local: std.ArrayList(Var) = .empty,
+stack: std.ArrayList(Var) = .empty,
 
-err_buf: [1024]u8,
-err: [*:0]const u8,
+err_buf: [1024]u8 = undefined,
+err: []const u8 = "",
 
 pub fn init() Self {
     const gpa = heap.c_allocator.create(heap.DebugAllocator(.{})) catch @panic("failed to create the allocator");
     gpa.* = .init;
-    return .{
-        .gpa = gpa,
-        .allocator = gpa.allocator(),
-        .vars = .{},
-        .stack = .{},
-        .err_buf = [_]u8{0} ** 1024,
-        .err = "",
-    };
+    return .{ .gpa = gpa, .allocator = gpa.allocator() };
 }
 
 pub fn deinit(self: *Self) void {
     self.vars.deinit(self.allocator);
+    self.local.deinit(self.allocator);
     self.stack.deinit(self.allocator);
     debug.assert(self.gpa.deinit() == .ok);
     heap.c_allocator.destroy(self.gpa);
 }
 
-pub fn execute(self: *Self, bytecode: []const u8) !void {
+pub fn execute(self: *Self, bytecode: []const u8) Error!void {
     defer {
         debug.print("vars: {any}\n", .{self.vars.items});
         debug.print("stack: {any}\n", .{self.stack.items});
@@ -56,31 +54,38 @@ pub fn execute(self: *Self, bytecode: []const u8) !void {
                 const lhs = switch (self.pop()) {
                     .i64 => |v| v,
                     else => |v| {
-                        self.err = try fmt.bufPrintZ(
+                        self.err = fmt.bufPrint(
                             &self.err_buf,
                             "evaluating `add`: expecting `i64`, found `{t}`",
                             .{v},
-                        );
-                        return error.Runtime;
+                        ) catch unreachable;
+                        return Error.Runtime;
                     },
                 };
                 const rhs = switch (self.pop()) {
                     .i64 => |v| v,
                     else => |v| {
-                        self.err = try fmt.bufPrintZ(
+                        self.err = fmt.bufPrint(
                             &self.err_buf,
                             "evaluating `add`: expecting `i64`, found `{t}`",
                             .{v},
-                        );
-                        return error.Runtime;
+                        ) catch unreachable;
+                        return Error.Runtime;
                     },
                 };
                 debug.print("{}\n", .{lhs + rhs});
             },
             .push => {
-                try self.stack.append(
-                    self.allocator,
+                try self.pushToList(
                     self.vars.items[mem.readVarInt(u16, bytecode[i + 1 .. i + 3], .little)],
+                    &self.stack,
+                );
+                i += 2;
+            },
+            .load => {
+                try self.pushToList(
+                    self.stack.items[mem.readVarInt(u16, bytecode[i + 1 .. i + 3], .little)],
+                    &self.stack,
                 );
                 i += 2;
             },
@@ -88,18 +93,26 @@ pub fn execute(self: *Self, bytecode: []const u8) !void {
     }
 }
 
-pub fn addVar(self: *Self, variable: Var) !u16 {
-    if (self.vars.items.len > math.maxInt(u16)) {
-        self.err = fmt.comptimePrint(
+pub fn addVar(self: *Self, variable: Var) Error!u16 {
+    if (self.vars.items.len >= max_slot_size) {
+        self.err = fmt.bufPrint(
+            &self.err_buf,
             "adding variable to memory: max variable number exceeded, which is `{}`",
-            .{math.maxInt(u16)},
-        );
-        return error.MaxVariableNumberExceeded;
+            .{max_slot_size},
+        ) catch unreachable;
+        return Error.MaxVariableNumberExceeded;
     }
-    try self.vars.append(self.allocator, variable);
+    try self.pushToList(variable, &self.vars);
     return @as(u16, @intCast(self.vars.items.len - 1));
 }
 
 fn pop(self: *Self) Var {
     return self.stack.pop().?;
+}
+
+fn pushToList(self: *Self, x: Var, xs: *std.ArrayList(Var)) !void {
+    xs.append(self.allocator, x) catch |err| {
+        self.err = fmt.bufPrint(&self.err_buf, "OOM", .{}) catch unreachable;
+        return err;
+    };
 }
---
+++ vm/src/root.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const heap = std.heap;
+
+const VM = @import("VM.zig");
+
+/// ## Error
+///
+/// Return `null` if allocation failed.
+export fn init() ?*VM {
+    const vm = heap.c_allocator.create(VM) catch return null;
+    vm.* = VM.init();
+    return vm;
+}
+
+export fn deinit(self: *VM) void {
+    self.deinit();
+    heap.c_allocator.destroy(self);
+}
+
+export fn err(self: *VM) [*:0]const u8 {
+    self.err_buf[self.err.len] = 0;
+    return self.err_buf[0..self.err.len :0];
+}
+
+/// ## Error
+///
+/// Return `65535` if `vars` is full.
+export fn addI64(self: *VM, variable: i64) u16 {
+    return self.addVar(.{ .i64 = variable }) catch VM.max_slot_size;
+}
+
+/// ## Error
+///
+/// Return `false` if error occurred.
+export fn execute(self: *VM, bytecode: [*]const u8, len: u64) bool {
+    self.execute(bytecode[0..len]) catch return false;
+    return true;
+}
