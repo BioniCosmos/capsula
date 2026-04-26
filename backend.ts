6be443d3bc206744b0872c35c17dbeaa2f3d3382
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-04-27 01:31:55 +0800

    Update backend

--- backend.ts
+++ backend.ts
@@ -1,10 +1,19 @@
-import { dlopen, FFIType, suffix } from 'bun:ffi'
-import { env } from './env'
-import { isList, type List } from './list'
+import { Instruction, serialize } from './bytecode'
+import { BytecodeVMEnv, env } from './env'
+import { isList } from './list'
 import { evaluate } from './native'
 import { isNumber } from './number'
 import { car, cdr } from './pair'
-import { isNil, isSymbol, typeOf, type SExpr } from './types'
+import { isString } from './string'
+import {
+  BytecodeCompiler,
+  isBoolean,
+  isNil,
+  isSymbol,
+  typeOf,
+  type SExpr,
+} from './types'
+import { VM } from './vm'
 
 interface Backend<T> {
   compile(source: SExpr[]): T
@@ -18,62 +27,60 @@ export class TreeWalk implements Backend<SExpr[]> {
 
   execute(artifact: SExpr[]): void {
     for (const expr of artifact) {
-      evaluate(expr, env)
+      const result = evaluate(expr, env)
+      if (result !== null) {
+        console.log(result)
+      }
     }
   }
 }
 
-export class BytecodeVM implements Backend<[Uint8Array, number]> {
-  compile(source: SExpr[]): [Uint8Array, number] {
-    const bytecode = new ArrayBuffer(1024)
-    const view = new DataView(bytecode)
-    let i = 0
-    for (const expr of source) {
-      if (isList(expr) && !isNil(expr)) {
-        let exprs = expr
+export class BytecodeVM implements Backend<Instruction[]> {
+  readonly #vm: VM
+  readonly env = new BytecodeVMEnv()
 
-        const sym = car(exprs)
-        if (!isSymbol(sym)) {
-          throw Error(`compiling: expecting symbol, found \`${typeOf(sym)}\``)
-        }
-        if (sym.value === '+') {
-          view.setUint8(i++, 0)
-        }
-
-        let tail = cdr(exprs) as List
-        if (isNil(tail)) {
-          throw Error(`compiling: missing arguments`)
-        }
-        exprs = tail
-
-        const lhs = car(exprs)
-        if (!isNumber(lhs)) {
-          throw Error(`compiling: expecting number, found \`${typeOf(lhs)}\``)
-        }
-        view.setBigInt64(i, BigInt(lhs), true)
-        i += 8
-
-        tail = cdr(exprs) as List
-        if (isNil(tail)) {
-          throw Error(`compiling: missing arguments`)
-        }
-        exprs = tail
-
-        const rhs = car(exprs)
-        if (!isNumber(rhs)) {
-          throw Error(`compiling: expecting number, found \`${typeOf(rhs)}\``)
-        }
-        view.setBigInt64(i, BigInt(rhs), true)
-        i += 8
-      }
-    }
-    return [new Uint8Array(bytecode), i]
+  constructor() {
+    this.#vm = new VM()
   }
 
-  execute(artifact: [Uint8Array, number]): void {
-    const lib = dlopen(`vm/libroot.${suffix}`, {
-      execute: { args: [FFIType.ptr, FFIType.u64], returns: FFIType.void },
-    })
-    lib.symbols.execute(...artifact)
+  [Symbol.dispose]() {
+    this.#vm[Symbol.dispose]()
+  }
+
+  compile(source: SExpr[]) {
+    const bytecode = Array.of<Instruction>()
+    for (const expr of source) {
+      bytecode.push(...this.compileExpr(expr))
+    }
+    return bytecode
+  }
+
+  execute(artifact: Instruction[]): void {
+    this.#vm.execute(serialize(artifact))
+  }
+
+  compileExpr(expr: SExpr) {
+    if (isNil(expr) || isBoolean(expr) || isNumber(expr) || isString(expr)) {
+      return [Instruction.Push(this.#vm.addVar(expr))]
+    }
+    if (isSymbol(expr)) {
+      const value = this.env.lookup(expr.value)
+      if (typeof value !== 'number') {
+        throw Error('TODO: `BytecodeCompiler.toString()`')
+      }
+      return [Instruction.Load(value)]
+    }
+    if (isList(expr)) {
+      const sym = car(expr)
+      if (!isSymbol(sym)) {
+        throw Error(`compiling: expecting symbol, found \`${typeOf(sym)}\``)
+      }
+      const compiler = this.env.lookup(sym.value)
+      if (!(compiler instanceof BytecodeCompiler)) {
+        throw Error('compiling: not callable')
+      }
+      return compiler.compile(this, cdr(expr))
+    }
+    throw Error(`compiling: unexpected \`${expr}\``)
   }
 }
