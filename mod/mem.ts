b3a279e29f2f43d8848dac1144b892c129109354
Hane Velarith <41363844+BioniCosmos@users.noreply.github.com>
2026-08-21 18:04:57 +0800

    QBE: simplify `mem`

--- mod/mem.ts
+++ mod/mem.ts
@@ -13,42 +13,36 @@ class Alloc implements QBECompiler {
     const x = ctx.compileExpr(cell.expr.car[1], env)!
     ctx.emit(`call $gc_retain(l ${x})`)
 
-    const result = env.defineTemp()
+    const result = ctx.if(
+      () => ctx.isArray(x, env),
+      () => {
+        const result = ctx.defineTemp(`alloc8 24`, env)
+        ctx.emit(`blit ${ctx.unwrapArray(x, env)}, ${result}, 24`)
 
-    const scalarBranch = env.defineBlock()
-    const arrayBranch = env.defineBlock()
-    const end = env.defineBlock()
-    ctx.emit(`jnz ${ctx.isArray(x, env)}, ${arrayBranch}, ${scalarBranch}`)
+        const type = env.defineTemp()
+        ctx.emit(`${type} =l loadl ${result}`)
+        ctx.emit(`${type} =l or ${type}, ${1n << 63n}`)
+        ctx.emit(`storel ${type}, ${result}`)
 
-    ctx.emit(scalarBranch)
-    ctx.emit(`${result} =l call $gc_alloc(l 8)`)
-    ctx.emit(`storel ${x}, ${result}`)
-    ctx.emit(`jmp ${end}`)
+        const dataSize = ctx.defineTemp(
+          `mul ${ctx.arrayLen(result, env)}, 8`,
+          env,
+        )
+        const newData = ctx.defineTemp(`$gc_alloc(l ${dataSize})`, env)
+        const ptrField = ctx.defineTemp(`add ${result}, 16`, env)
+        const oldData = ctx.defineTemp(`loadl ${ptrField}`, env)
+        ctx.emit(`call $memcpy(l ${newData}, l ${oldData}, l ${dataSize})`)
+        ctx.emit(`storel ${newData}, ${ptrField}`)
+        return ctx.wrapArray(result, env)
+      },
+      () => {
+        const result = ctx.defineTemp(`call $gc_alloc(l 8)`, env)
+        ctx.emit(`storel ${x}, ${result}`)
+        return result
+      },
+      env,
+    )
 
-    ctx.emit(arrayBranch)
-    ctx.emit(`${result} =l alloc8 24`)
-    ctx.emit(`blit ${ctx.unwrapArray(x, env)}, ${result}, 24`)
-
-    const type = env.defineTemp()
-    ctx.emit(`${type} =l loadl ${result}`)
-    ctx.emit(`${type} =l or ${type}, ${1n << 63n}`)
-    ctx.emit(`storel ${type}, ${result}`)
-
-    const ptr = ctx.defineTemp(`add ${result}, 8`, env)
-    const dataSize = ctx.defineTemp(`loadl ${ptr}`, env)
-    ctx.emit(`${dataSize} =l mul ${dataSize}, 8`)
-
-    const newData = env.defineTemp()
-    ctx.emit(`${newData} =l call $gc_alloc(l ${dataSize})`)
-    ctx.emit(`${ptr} =l add ${result}, 16`)
-    const oldData = env.defineTemp()
-    ctx.emit(`${oldData} =l loadl ${ptr}`)
-    ctx.emit(`call $memcpy(l ${newData}, l ${oldData}, l ${dataSize})`)
-    ctx.emit(`storel ${newData}, ${ptr}`)
-    ctx.emit(`${result} =l or ${result}, ${qbeConst.array}`)
-    ctx.emit(`jmp ${end}`)
-
-    ctx.emit(end)
     ctx.emit(`call $gc_release(l ${x})`)
     return result
   }
@@ -57,25 +51,21 @@ class Alloc implements QBECompiler {
 class Free implements QBECompiler {
   compileToQBE(ctx: QBEBackend, cell: ASTNode<SExprCell>, env: QBEEnv) {
     const x = ctx.compileExpr(cell.expr.car[1], env)
-
-    const scalarBranch = env.defineBlock()
-    const arrayBranch = env.defineBlock()
-    const end = env.defineBlock()
-    ctx.emit(`jnz ${ctx.isArray(x, env)}, ${arrayBranch}, ${scalarBranch}`)
-
-    ctx.emit(scalarBranch)
-    ctx.emit(`call $free(l ${x})`)
-    ctx.emit(`jmp ${end}`)
-
-    ctx.emit(arrayBranch)
-    const ptr = env.defineTemp()
-    ctx.emit(`${ptr} =l add ${ctx.unwrapArray(x, env)}, 16`)
-    ctx.emit(`${ptr} =l loadl ${ptr}`)
-    ctx.emit(`call $free(l ${ptr})`)
-    ctx.emit(`jmp ${end}`)
-
-    ctx.emit(end)
-    return qbeConst.Unit
+    return ctx.if(
+      () => ctx.isArray(x, env),
+      () => {
+        const p = env.defineTemp()
+        ctx.emit(`${p} =l add ${ctx.unwrapArray(x, env)}, 16`)
+        ctx.emit(`${p} =l loadl ${p}`)
+        ctx.emit(`call $free(l ${p})`)
+        return qbeConst.Unit
+      },
+      () => {
+        ctx.emit(`call $free(l ${x})`)
+        return qbeConst.Unit
+      },
+      env,
+    )
   }
 }
 
